df596a880c8a6dc4517367c51562025a5816057e
mayesha35 <119776219+mayesha35@users.noreply.github.com>
2023-10-17 00:00:40 +0600

    displaying-foodElement

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //Game variables and constants
+let board = document.querySelector("#board");
 let direction = {
   x: 0,
   y: 0,
@@ -8,24 +9,33 @@ const gameOverSound = new Audio("../music/gameover.mp3");
 const moveSound = new Audio("../music/move.mp3");
 const gameBGSound = new Audio("../music/music.mp3");
 let speed = 2;
-let lastPrinttime = 0;
-let snakeArr = {
-  x: 13,
-  y: 15,
-};
+let lastPrintTime = 0;
+let snakeArr = [
+  {
+    x: 13,
+    y: 15,
+  },
+];
+let food = { x: 6, y: 7 };
 //Game Functions
 function main(currentTime) {
   window.requestAnimationFrame(main);
-  if ((currentTime - lastPrinttime) / 1000 > 1 / speed) {
+  if ((currentTime - lastPrintTime) / 1000 < 1 / speed) {
     return;
   }
-  currentTime = lastPrinttime;
+  lastPrintTime = currentTime;
   gameEngine();
 }
 
 function gameEngine() {
   //part1:updating the snake array(body of the snake) & food array
   //part2:render/display the snake and food on screen
+  // Display the food
+  foodElement = document.createElement("div");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food");
+  board.appendChild(foodElement);
 }
 
 //Game Main Logic starts here
